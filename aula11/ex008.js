6bde5b7ce33edf9c5fffe2b5440dc0ba154cc6a9
Klebson Almeida Oliveira <107286029+klebsonstifler@users.noreply.github.com>
2023-04-29 16:25:11 -0300

    hora finish

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -3,4 +3,4 @@ console.log(`A velocidade do seu carro é ${vel}Km/h`)
 if (vel > 60) {
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log(`Dirija sempre usando sinto de segurança`)
+console.log(`Dirija sempre usando cinto de segurança`)
